98c709c06c2ec86c3be13aad0efcb10b2ef93689
eunwoo <aufcl4858@naver.com>
2019-04-05 01:18:46 +0900

    조정 탭 디자인 업데이트

--- src/adjust/component/TotalSpedingStatusView.tsx
+++ src/adjust/component/TotalSpedingStatusView.tsx
@@ -6,10 +6,19 @@ import { tagList } from 'model/item/Tag';
 
 // view
 import FinguHorizontoalBar from 'common/component/chart/FinguHorizontalBar';
+import FinguPieChart from 'common/component/chart/FinguPieChart';
+
+// util
+import StringUtil from 'common/utils/StringUtil';
+
+// styles
+import styles from './TotalSpedingStatusView.scss';
 
 interface TotalSpedingStatusViewProps {
   totalSpending: TotalSpending;
   isAfterAdjust: boolean;
+  budget: number;
+  totalSpendingAmount: number;
 }
 
 class TotalSpedingStatusView extends Component<
@@ -17,17 +26,48 @@ class TotalSpedingStatusView extends Component<
   {}
 > {
   render() {
-    const { totalSpending } = this.props;
+    const { totalSpending, budget } = this.props;
+    const homeSpending = totalSpending['고정지출'] + totalSpending['변동지출'];
     if (totalSpending === undefined) return null;
     return (
       <div>
-        {this.renderStatusTable()}
-        <FinguHorizontoalBar
-          labels={['저축투자', '지출합계', '미파악지출']}
-          data={this.getChartData()}
-          backgroundColor={['#0088FE', '#00C49F', '#FFBB28']}
-          hoverBackgroundColor={['#0088FE', '#00C49F', '#FFBB28']}
+        <div className={styles.title}>
+          유출 조정 {this.getPreviousPostText()}
+        </div>
+        <div className={styles.statusBox}>
+          {this.renderStatusTable()}
+          <FinguHorizontoalBar
+            className={styles.barChart}
+            labels={['저축투자', '지출합계', '미파악지출']}
+            data={this.getChartData()}
+            backgroundColor={['#0088FE', '#00C49F', '#FFBB28']}
+            hoverBackgroundColor={['#0088FE', '#00C49F', '#FFBB28']}
+          />
+        </div>
+        <div className={styles.title}>
+          소득대비 가계지출 비중 - 조정 {this.getPreviousPostText()}
+        </div>
+        <FinguPieChart
+          className={styles.pieChart}
+          labels={['가계지출', '기타']}
+          data={[homeSpending, budget - homeSpending]}
+          backgroundColor={['#0088FE', '#00C49F']}
+          hoverBackgroundColor={['#0088FE', '#00C49F']}
         />
+        <div>
+          <div className={styles.rightSpendingBox}>
+            <div className={styles.spendingTitle}>
+              조정 {this.getPreviousPostText()} 가계 지출 금액
+            </div>
+            <div className={styles.spendingAmount}>{homeSpending}원</div>
+          </div>
+          <div className={styles.leftSpendingBox}>
+            <div className={styles.spendingTitle}>적정 가계 지출 금액</div>
+            <div className={styles.spendingAmount}>
+              {budget === 0 ? 0 : Math.round(budget / 2)}원
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
@@ -40,14 +80,20 @@ class TotalSpedingStatusView extends Component<
           {tagList.map((tag, index) => {
             return (
               <tr key={index}>
-                <td>{tag}</td>
-                <td>{totalSpending[tag]}</td>
+                <td className={styles.tableRowKey}>{tag}</td>
+                <td className={styles.tableRowValue}>
+                  {StringUtil.getCurrencyValue(totalSpending[tag])}원
+                </td>
               </tr>
             );
           })}
         </tbody>
       </table>
     );
+  }
+
+  private getPreviousPostText() {
+    return this.props.isAfterAdjust ? '후' : '전';
   }
 
   private getChartData(): number[] {
--- src/adjust/screen/Adjust.tsx
+++ src/adjust/screen/Adjust.tsx
@@ -12,13 +12,17 @@ import { tagList } from 'model/item/Tag';
 import Container from 'common/component/container/Container';
 import TotalSpendingStatusView from 'adjust/component/TotalSpedingStatusView';
 
+// util
+import StringUtil from 'common/utils/StringUtil'
+
 // styles
 import styles from './Adjust.scss';
 
 interface AdjustState {
   baseSpending: TotalSpending;
   adjustSpending: TotalSpending;
-  selected;
+  totalSpendingAmount: number;
+  budget: number;
 }
 
 class Adjust extends Component<{}, AdjustState> {
@@ -36,12 +40,19 @@ class Adjust extends Component<{}, AdjustState> {
     this.setState({
       ...this.state,
       baseSpending: totalSpending,
-      adjustSpending: totalSpending
+      adjustSpending: totalSpending,
+      totalSpendingAmount: user.getTotalSpending(),
+      budget: user.budget
     });
   }
 
   render() {
-    const { baseSpending, adjustSpending } = this.state;
+    const {
+      baseSpending,
+      adjustSpending,
+      budget,
+      totalSpendingAmount
+    } = this.state;
     return (
       <Container className={styles.container}>
         <div className={styles.title}>
@@ -50,17 +61,36 @@ class Adjust extends Component<{}, AdjustState> {
         <div className={styles.description}>
           항목을 꼼꼼히 살피고, 줄일수 있는 항목을 찾아봐요.
         </div>
+        <div className={styles.budgetBox}>
+            <div className={styles.budgetTitle}>
+                유입합계
+            </div>
+            <div className={styles.budgetAmount}>
+                {StringUtil.getCurrencyValue(budget)}원
+            </div>
+        </div>
         <div className={styles.beforeAdjustBox}>
           <TotalSpendingStatusView
             totalSpending={baseSpending}
             isAfterAdjust={false}
+            budget={budget}
+            totalSpendingAmount={totalSpendingAmount}
           />
         </div>
         <div className={styles.afterAdjustBox}>
           <TotalSpendingStatusView
             totalSpending={adjustSpending}
             isAfterAdjust={true}
+            budget={budget}
+            totalSpendingAmount={totalSpendingAmount}
           />
+        </div>
+        <div className={styles.descBox}>
+          <div className={styles.descTitle}>가계지출 = 고정지출 + 변동지출</div>
+          <div className={styles.descSubtitle}>
+            가계지출은 일반적으로 총 소득 대비 <b>50%</b> 이하를 유지하는것이
+            바람직합니다
+          </div>
         </div>
       </Container>
     );
