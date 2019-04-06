4e27982c88bcefdda5967d2ce5fb0080633770cc
eunwoo <aufcl4858@naver.com>
2019-04-06 15:06:55 +0900

    인트로 화면 디자인, 애니메이션 추가

--- src/basicinfo/screen/BasicInfo.tsx
+++ src/basicinfo/screen/BasicInfo.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { Component } from 'react';
+import classNames from 'classnames';
 
 import User from '../../model/user/User';
 import UserController from '../../controller/UserController';
@@ -25,25 +26,30 @@ class BasicInfo extends Component<{}, BasicInfoState> {
   render() {
     const { user } = this.state;
     return (
-      <div className={styles.container}>
-        <div className={styles.title}>
-          당신의 금융친구 <span>Fingu</span>
+      <div
+        className={styles.mainScreen}
+        style={{ background: require('common/assets/images/main_bg.jpg') }}
+      >
+        <div className={classNames(styles.container)}>
+          <div className={styles.title}>
+            당신의 금융친구 <span>FINGU</span>
+          </div>
+          <Input
+            className={styles.nameInput}
+            label="이름"
+            type="text"
+            value={user.name}
+            onChange={this.onChangeUsername.bind(this)}
+          />
+          <Input
+            className={styles.ageInput}
+            label="월 단위 예산/수입"
+            type="number"
+            value={user.budget}
+            onChange={this.onChangeUserAge.bind(this)}
+          />
+          <Button onClick={this.onClickSubmitButton.bind(this)} text="확인" />
         </div>
-        <Input
-          className={styles.nameInput}
-          label="이름"
-          type="text"
-          value={user.name}
-          onChange={this.onChangeUsername.bind(this)}
-        />
-        <Input
-          className={styles.ageInput}
-          label="월 단위 예산/수입"
-          type="number"
-          value={user.budget}
-          onChange={this.onChangeUserAge.bind(this)}
-        />
-        <Button onClick={this.onClickSubmitButton.bind(this)} text="확인" />
       </div>
     );
   }
